59ea9c3f354fb9da814e13de47cee2fff6e751d2
Jeevansh Grover <groverjeevansh0243@gmail.com>
2025-08-11 22:54:34 +0530

    implement signup functionality and create signup page

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,40 @@
+import {create} from 'zustand';
+import { axiosInstance } from '../lib/axios.js';
+import toast from 'react-hot-toast';
+
+
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+
+    isCheckingAuth: true,
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/checkAuth");
+            set({
+                authUser: res.data
+            });
+        } catch (error) {
+            console.log("error in checkAuth:", error);
+            set({ authUser: null})
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    },
+
+    signup: async (data) => {
+        try {
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({ authUser: res.data });
+            toast.success("Account created successfully");
+        } catch (error) {
+            toast.error(error.response?.data?.message || "Signup failed");
+        } finally {
+            set({ isSigningUp: false });
+        }
+    }
+    
+}))
